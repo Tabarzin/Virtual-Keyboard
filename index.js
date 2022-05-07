14f842d4436eda51ebb3a53faf5406526f0fedb1
Rustam <tabarzin@gmail.com>
2022-05-07 20:27:46 +0300

    feat: add html and css

---
+++ index.js
@@ -0,0 +1,31 @@
+let div = document.createElement('textarea');
+document.body.appendChild(div);
+
+let keyboard = document.createElement('div');
+keyboard.className = 'keyboard';
+document.body.append(keyboard);
+
+
+const Keyboard = {
+
+  elements: {
+
+    main: null,
+    container: null,
+    keys: []
+
+
+  },
+
+  eventHandlers: {
+    oninput: null,
+  },
+
+
+  
+
+
+
+
+
+};
